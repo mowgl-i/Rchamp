ad51768a27117df166015179d0742a912e071e0a
mowgli <46568362+mowgl-i@users.noreply.github.com>
2021-07-12 23:33:52 -0400

    live socket start

---
+++ R/live_socket.R
@@ -0,0 +1,12 @@
+server = 'irc.chat.twitch.tv'
+port = 6667
+nickname = 'lsf_mowgli_chat'
+token = 'oauth:vxcow8r2e4iqtuu34vbzso9bbptz2y'
+
+
+live_socket <- function(channel){
+
+ con <-socketConnection(port = port,server=T)
+
+
+}
